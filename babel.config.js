d7abd080108aa9101f2a69384f06d285837d7237
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2026-04-14 19:36:36 +0800

    Added line to enable import/export instead of require/module.exports

---
+++ babel.config.js
@@ -0,0 +1,5 @@
+// this line will enable you to write 'import/export' the usual way instead
+// of 'require/module.exports'
+export default {
+  presets: [["@babel/preset-env", { targets: { node: "current" } }]],
+};
